7be994bf8f6043c1174657890481d1b32db4cbe0
RefaelSheffer <Refael214@gmail.com>
2025-09-12 10:41:10 +0300

    Add ONVIF telemetry check with CGI fallback

--- tests/test_any_ptz_client.py
+++ tests/test_any_ptz_client.py
@@ -16,7 +16,7 @@ def test_any_ptz_client_fallback(monkeypatch):
         def stop(self):
             calls['onvif_stop'] = True
         def last(self):
-            return 1
+            return any_ptz_client.PTZReading()
 
     class DummyCgi:
         def __init__(self, *a, **kw):
@@ -26,7 +26,7 @@ def test_any_ptz_client_fallback(monkeypatch):
         def stop(self):
             calls['cgi_stop'] = True
         def last(self):
-            return 2
+            return any_ptz_client.PTZReading(pan_deg=2.0)
         poll_dt = 0.2
 
     monkeypatch.setattr(any_ptz_client, 'OnvifPTZClient', DummyOnvif)
@@ -34,7 +34,7 @@ def test_any_ptz_client_fallback(monkeypatch):
     c = any_ptz_client.AnyPTZClient('h', 80, 'u', 'p')
     c.start()
     assert c.mode == 'cgi'
-    assert c.last() == 2
+    assert c.last().pan_deg == 2.0
     assert c.poll_dt == DummyCgi.poll_dt
     c.stop()
     assert calls.get('cgi_stop')
@@ -50,17 +50,55 @@ def test_any_ptz_client_onvif(monkeypatch):
         def stop(self):
             pass
         def last(self):
-            return 3
+            return any_ptz_client.PTZReading(pan_deg=1.0)
 
     class DummyCgi:
         def __init__(self, *a, **kw):
             raise AssertionError('should not be used')
 
+    monkeypatch.setattr(any_ptz_client.time, 'sleep', lambda x: None)
     monkeypatch.setattr(any_ptz_client, 'OnvifPTZClient', DummyOnvif)
     monkeypatch.setattr(any_ptz_client, 'PtzCgiThread', DummyCgi)
     c = any_ptz_client.AnyPTZClient('h', 80, 'u', 'p')
     c.start()
     assert c.mode == 'onvif'
-    assert c.last() == 3
+    assert c.last().pan_deg == 1.0
     assert c.poll_dt == DummyOnvif.poll_dt
+    c.stop()
+
+
+def test_any_ptz_client_empty_onvif(monkeypatch, capsys):
+    class DummyOnvif:
+        poll_dt = 0.1
+        def __init__(self, *a, **kw):
+            pass
+        def start(self):
+            pass
+        def stop(self):
+            pass
+        def last(self):
+            return any_ptz_client.PTZReading()
+
+    class DummyCgi:
+        poll_dt = 0.2
+        def __init__(self, *a, **kw):
+            pass
+        def start(self):
+            pass
+        def stop(self):
+            pass
+        def last(self):
+            return any_ptz_client.PTZReading(pan_deg=5.0)
+
+    monkeypatch.setattr(any_ptz_client.time, 'sleep', lambda x: None)
+    monkeypatch.setattr(any_ptz_client, 'OnvifPTZClient', DummyOnvif)
+    monkeypatch.setattr(any_ptz_client, 'PtzCgiThread', DummyCgi)
+
+    c = any_ptz_client.AnyPTZClient('h', 80, 'u', 'p')
+    c.start()
+    assert c.mode == 'cgi'
+    assert c.last().pan_deg == 5.0
+    assert c.poll_dt == DummyCgi.poll_dt
+    out = capsys.readouterr().out
+    assert 'ONVIF telemetry empty' in out
     c.stop()
